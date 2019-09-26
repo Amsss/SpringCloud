a2c8ec5b2d5bdfa6be80455148350bc444f245de
Amsss <906602554@qq.com>
2019-09-26 20:52:10 +0800

    springCloud Config

---
+++ spring-boot-config-client/src/main/java/com/firesoon/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.firesoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zhuzz
+ * @date: 2019-09-26 20:45
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ spring-boot-config-client/src/main/java/com/firesoon/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.firesoon.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: zhuzz
+ * @date: 2019-09-26 20:46
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
+}
